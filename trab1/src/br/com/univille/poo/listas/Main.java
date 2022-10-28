890c7e9a29d4a1349c0d25648d86df90af466e18
BrunoScholze2003 <104472351+BrunoScholze2003@users.noreply.github.com>
2022-10-28 19:37:20 -0300

    Add files via upload

---
+++ trab1/src/br/com/univille/poo/listas/Main.java
@@ -0,0 +1,11 @@
+package br.com.univille.poo.listas;
+
+import br.com.univille.poo.listas.ui.CadastroTarefasJanela;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CadastroTarefasJanela cadastroTarefasJanela = new CadastroTarefasJanela();
+    }
+
+}
